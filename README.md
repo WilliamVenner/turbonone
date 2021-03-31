c30592ecedfc17f3bac99b413e31b03a228b1842
William Venner <william@venner.io>
2021-03-31 23:32:49 +0100

    Update README.md and docs

--- README.md
+++ README.md
@@ -2,14 +2,30 @@
 
 Tiny macro for calling functions with `Option<T>` arguments.
 
-## Example
+## The Problem
 
 ```rust
-fn my_function<T>(arg: Option<T>) {
-    ...
+fn my_function<T>(arg: Option<T>) -> &'static str {
+    "Works!"
 }
 
 my_function(None); // cannot infer type for type parameter `T` declared on the associated function `my_function`
+my_function(Some("An argument")); // Works!
+```
+
+## The Solution
+
+```rust
+// Rust 2015
+#[macro_use] extern crate turbonone;
+
+// Rust 2018
+use turbonone::turbonone;
+
+fn my_function<T>(arg: Option<T>) -> &'static str {
+    "Works!"
+}
+
 my_function(turbonone!()); // Works!
-my_function(Some(...)); // Works!
+my_function(Some("An argument")); // Works!
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! Tiny macro for calling functions with `Option<T>` arguments.
+//!
+//! ## The Problem
+//!
+//! ```ignore
+//! # extern crate core;
+//! # extern crate turbonone;
+//! # use turbonone::turbonone;
+//! fn my_function<T>(arg: Option<T>) -> &'static str {
+//!     "Works!"
+//! }
+//! 
+//! my_function(None); // cannot infer type for type parameter `T` declared on the associated function `my_function`
+//! my_function(Some("An argument")); // Works!
+//! ```
+//! 
+//! ## The Solution
+//! 
+//! ```rust
+//! # extern crate core;
+//! # extern crate turbonone;
+//! # #[macro_use]
+//! use turbonone::turbonone;
+//! 
+//! fn my_function<T>(arg: Option<T>) -> &'static str {
+//!     "Works!"
+//! }
+//! 
+//! my_function(turbonone!()); // Works!
+//! my_function(Some("An argument")); // Works!
+//! ```
+
 #![no_std]
 #[macro_export]
+/// ```rust
+/// #![no_std]
+/// #[macro_export]
+/// macro_rules! turbonone { () => { ::core::option::Option::None::<()> } }
+/// ```
 macro_rules! turbonone { () => { ::core::option::Option::None::<()> } }
